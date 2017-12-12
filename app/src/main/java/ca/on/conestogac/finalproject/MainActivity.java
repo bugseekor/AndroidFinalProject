e4b4a689bc09473b468341eb68b18859e6ffbcb1
bugseekor <compukin@gmail.com>
2017-12-12 13:11:08 -0500

    Initial commit

---
+++ app/src/main/java/ca/on/conestogac/finalproject/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.on.conestogac.finalproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
